d69954349aa4f33982942069ff17d65bddf626f1
Elemen <3242388085@qq.com>
2025-09-29 21:49:28 +0800

    del:deleted some unnecessary config

--- config/index.ts
+++ config/index.ts
@@ -1,18 +1,16 @@
 import { createConfig, http } from 'wagmi';
-import { sepolia } from 'wagmi/chains';
-import { getDefaultConfig } from 'connectkit';
+import { sepolia,mainnet } from 'wagmi/chains';
 import counterArtifact from '@/abi/counter.json';
 
 // 1. Wagmi 配置
-export const config = createConfig(getDefaultConfig({
-    appName: 'Solidity Next.js Template',
-    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
-    chains: [sepolia],
+export const config = createConfig({
+    chains: [sepolia,mainnet],
     transports: {
         [sepolia.id]: http(),
+        [mainnet.id]: http(),
     },
-}));
+});
 
 // 2. 合约配置
-export const counterAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `${string}`;
+export const counterAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`;
 export const counterAbi = counterArtifact.abi;
